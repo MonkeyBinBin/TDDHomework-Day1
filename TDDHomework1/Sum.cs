8692be7d4ca99dc1029de857a4592debf46d11e7
Wenpin Liao <thisisbinbin@gmail.com>
2016-01-10 11:52:08 +0800

    first commit

---
+++ TDDHomework1/Sum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDHomework1
+{
+    public class Sum
+    {
+        public int[] FieldSumInGroupByRowCount<T>(int rowCount, string fieldName, IEnumerable<T> data)
+        {
+            if (data == null || data.Count() == 0)
+                return new int[] { };
+
+            Type type = typeof(T);
+            var propertyInfo = type.GetProperty(fieldName);
+
+            List<int> resultData = new List<int>();
+            int calCount = 0;
+            int sum=0;
+            foreach (var item in data)
+            {
+                sum += (int)propertyInfo.GetValue(item);
+                calCount++;
+
+                if (calCount == rowCount)
+                {
+                    resultData.Add(sum);
+                    calCount = 0;
+                    sum = 0;
+                }
+            }
+            if (calCount != 0)
+                resultData.Add(sum);
+
+            return resultData.ToArray();
+        }
+    }
+}
