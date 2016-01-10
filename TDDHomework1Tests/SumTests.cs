2b3df0b52adf291150f7b94b0c1cbb2e2eddb9ea
Wenpin Liao <thisisbinbin@gmail.com>
2016-01-10 23:13:37 +0800

    issue #1 fixed

--- TDDHomework1Tests/SumTests.cs
+++ TDDHomework1Tests/SumTests.cs
@@ -16,39 +16,27 @@ namespace TDDHomework1.Tests
         {
             //arrange
             var target = new Sum();
-            var groupFirst = new List<Order>()
+            var groupRowSize = 3;
+            var sumField = "Cost";
+            var orders = new List<Order>()
             {
                 new Order() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 },
                 new Order() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 },
-                new Order() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 }
-            };
-            var groupSecond = new List<Order>()
-            {
+                new Order() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 },
                 new Order() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 },
                 new Order() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 },
-                new Order() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 }
-            };
-            var groupThird = new List<Order>()
-            {
+                new Order() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 },
                 new Order() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 },
                 new Order() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 },
-                new Order() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 }
-            };
-            var groupFourth = new List<Order>()
-            {
+                new Order() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 },
                 new Order() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 },
                 new Order() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 }
             };
-            
-            var expected = new int[] { 
-                groupFirst.Select(o=>o.Cost).Sum(),
-                groupSecond.Select(o=>o.Cost).Sum(),
-                groupThird.Select(o=>o.Cost).Sum(),
-                groupFourth.Select(o=>o.Cost).Sum()
-            };
+
+            var expected = new int[] { 6, 15, 24, 21 };
 
             //act
-            var actual = target.FieldSumInGroupByRowCount<Order>(3, "Cost", groupFirst.Concat(groupSecond).Concat(groupThird).Concat(groupFourth));
+            var actual = target.FieldSumInGroupByRowCount<Order>(groupRowSize, sumField, orders);
 
             //assert            
             CollectionAssert.AreEqual(expected, actual);
@@ -59,35 +47,27 @@ namespace TDDHomework1.Tests
         {
             //arrange
             var target = new Sum();
-            var groupFirst = new List<Order>()
+            var groupRowSize = 4;
+            var sumField = "Revenue";
+            var orders = new List<Order>()
             {
                 new Order() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 },
                 new Order() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 },
                 new Order() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 },
-                new Order() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 }
-            };
-            var groupSecond = new List<Order>()
-            {                
+                new Order() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 },
                 new Order() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 },
                 new Order() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 },
                 new Order() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 },
-                new Order() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 }
-            };
-            var groupThird = new List<Order>()
-            {               
+                new Order() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 },
                 new Order() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 },
                 new Order() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 },
                 new Order() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 }
             };
-            
-            var expected = new int[] { 
-                groupFirst.Select(o=>o.Revenue).Sum(),
-                groupSecond.Select(o=>o.Revenue).Sum(),
-                groupThird.Select(o=>o.Revenue).Sum()
-            };
+
+            var expected = new int[] { 50, 66, 60 };
 
             //act
-            var actual = target.FieldSumInGroupByRowCount<Order>(4, "Revenue", groupFirst.Concat(groupSecond).Concat(groupThird));
+            var actual = target.FieldSumInGroupByRowCount<Order>(groupRowSize, sumField, orders);
 
             //assert
             CollectionAssert.AreEqual(expected, actual);
